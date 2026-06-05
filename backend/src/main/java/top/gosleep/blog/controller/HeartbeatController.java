e61fa6b96a48cad2c723c8ae6e04bf3254e0f3ef
mydefinition <2785268186@qq.com>
2026-06-05 18:27:04 +0800

    前端实现心跳机制

--- backend/src/main/java/top/gosleep/blog/controller/HeartbeatController.java
+++ backend/src/main/java/top/gosleep/blog/controller/HeartbeatController.java
@@ -1,6 +1,8 @@
 package top.gosleep.blog.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.gosleep.blog.common.Result;
@@ -10,7 +12,8 @@ import top.gosleep.blog.common.Result;
 @Tag(name = "心跳接口")
 public class HeartbeatController {
 
-    @RequestMapping
+    @GetMapping
+    @Operation(summary = "发送心跳")
     public Result<?> heartbeat() {
         return Result.ok();
     }
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,10 +8,19 @@
 <script setup lang="ts">
 import Navbar from '@/components/Navbar.vue'
 import { useSettingsStore } from '@/stores/settings'
-import { onMounted } from 'vue'
+import { onMounted, onBeforeUnmount  } from 'vue'
 
 const settings = useSettingsStore()
 onMounted(() => {
   settings.apply()
 })
+
+let timer: ReturnType<typeof setInterval>
+
+// 心跳机制
+onMounted(() => {
+  timer = setInterval(() => fetch('/api/heartbeat').catch(() => {}), 60 * 1000)
+})
+
+onBeforeUnmount(() => clearInterval(timer))
 </script>
